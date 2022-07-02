fa7f505cd5ade8d790c499135b971fffe536df8c
fladeia <ladeiadev@gmail.com>
2022-07-02 14:07:49 -0300

    added footer

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
 import { Video } from '../components/Video'
@@ -13,6 +14,7 @@ export const Event = () => {
 
         <Sidebar />
       </main>
+      <Footer />
     </div>
   )
 }
